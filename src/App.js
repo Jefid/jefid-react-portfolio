f362686b824ba614ccad16e3143ff38415e198f5
Jefid <99925114+Jefid@users.noreply.github.com>
2022-07-13 16:09:01 -0400

    troubleshooted errors with react and finally got my homepage to load/reflect async work

--- src/App.js
+++ src/App.js
@@ -2,41 +2,49 @@ import React, { useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import Nav from "./components/Nav";
-import Contact from "./components/Contact";
+import ContactForm from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
-
 function App() {
   const [categories] = useState([
     {
-      name: "about me",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
+      name: "About Me",
+      // description:
+      //   "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    { name: "portfolio", description: "Portraits of people in my life" },
-    { name: "contact", description: "Delicious delicacies" },
-    {
-      name: "resume",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
+    { name: "Projects", description: "Projects I've developed" },
+    { name: "Contact" },
   ]);
-
+  const [contactSelected, setContactSelected] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
   return (
-    <section className="m-5">
-      <Nav
+    <section className="my-5">
+      {/* <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav> */}
+          <main>
+        {!contactSelected ? (
+  <>
+    <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
-      <main>
-          <About></About>
+    <About></About>
+  </>
+) : (
+    <ContactForm></ContactForm>
+  )}
       </main>
     </section>
   );
 }
-
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,43 +1,35 @@
 import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
-
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected,
   } = props;
-
-
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
-
   return (
     <header className="flex-row px-1">
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
-              About
+          <a data-testid="about" onClick={() => setContactSelected(false)}>
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick('Contact')}>
-              Contact
-            </span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name
+                currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
             >
               <span
                 onClick={() => {
                   setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
@@ -49,5 +41,4 @@ function Nav(props) {
     </header>
   );
 }
-
 export default Nav;
