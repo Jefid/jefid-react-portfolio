1141ec71a26dca83924b76a122e7f637ae0ec445
Jefid <99925114+Jefid@users.noreply.github.com>
2022-07-13 15:42:41 -0400

    working on About Me page and nav bar in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import './App.css';
-import About from './components/About';
+import React, { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "about me",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portfolio", description: "Portraits of people in my life" },
+    { name: "contact", description: "Delicious delicacies" },
+    {
+      name: "resume",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div>
-    <main>
-      <About></About>
-    </main>
-  </div>
+    <section className="m-5">
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+          <About></About>
+      </main>
+    </section>
   );
 }
 
