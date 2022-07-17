5ac44dcafcda2743006f78ca751a77aa8c5209e6
Jefid <99925114+Jefid@users.noreply.github.com>
2022-07-17 12:51:03 -0400

    restructed code to reflect module 21

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
-import React, { useState } from "react";
-import "./App.css";
-import About from "./components/About";
-import Nav from "./components/Nav";
-import ContactForm from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-// import Portfolio from "./components/Portfolio";
-import Resume from "./components/Resume";
+mport React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+// components
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import SideNav from './components/SideNav';
+import Work from './components/Work';
+
 function App() {
-  const [categories] = useState([
-    {
-      name: "About Me",
-      // description:
-      //   "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "Portfolio", description: "Portfolio I've developed" },
-  
-  ]);
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-  const [contactSelected, setContactSelected] = useState(false);
   return (
-    <section className="my-5">
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-      {!contactSelected ? (
-  <>
-    <About></About>
-    {/* <Portfolio></Portfolio> */}
-  </>
-) : (
-    <ContactForm></ContactForm>
-  )}
-      </main>
-      </section>
+    <div className='App'>
+      <SideNav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/portfolio' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/work' element={<Work />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
+
 export default App;
