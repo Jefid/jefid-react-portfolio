9de5205856f8e03def01b521a0a7df29d1f454cf
Jefid <99925114+Jefid@users.noreply.github.com>
2022-07-17 13:29:47 -0400

    reformatted About index

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,50 @@
-import React from "react";
-import profileImage from "../../assets/cover/profile.jpg";
+import React from 'react';
+import profilePic from '..\assets\cover\profile.jpg';
+import './style.css';
+import { Container } from '@mui/material';
 
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">Jefferson Quandt</h1>
-      <img
-        src={profileImage}
-        className="my-2"
-        style={{ width: "30%" }}
-        alt="cover"
-      />
-    </section>
+    <div>
+      <h1>i'm wolf!</h1>
+
+      <Container id='project-card'>
+        <h3>My name is Jefferson Quandt</h3>
+        <Container>
+          <img
+            title='hangin in Malibu'
+            id='wolfpic'
+            alt='wolfpic'
+            src={profilePic}
+          />
+        </Container>
+        <p>
+          <ul>
+            <li>
+              I'm a full-stack JavaScript developer with a background in Management.
+            </li>
+            <li>
+              I love solving puzzles, enhancing code, and creating/collaborating
+              on projects.
+            </li>
+            <li>
+              I'm skilled in JavaScript, React, CSS, Node, MySql, NoSql, HTML, and Express.
+            </li>
+          </ul>
+        </p>
+      </Container>
+
+      <Container id='project-card'>
+        <p>
+          Current developer plans:
+          <ul>
+            <li>- honing in my skills on C++ and C# to be able to work with older code</li>
+            <li>- updating and improving old projects</li>
+            <li>- practicing algorithms</li>
+          </ul>
+        </p>
+      </Container>
+    </div>
   );
 }
 
